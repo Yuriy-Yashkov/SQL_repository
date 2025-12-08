d56dc1930076f623236250b02bc65832c3397ff6
Yuriy-Yashkov <58135208+Yuriy-Yashkov@users.noreply.github.com>
2025-12-08 11:03:45 +0300

    Update 02_poshiv.sql
    Добавлен "Бригадир"

--- SQL/02_poshiv.sql
+++ SQL/02_poshiv.sql
@@ -12,11 +12,12 @@ SELECT nsi_kld.nar                               'Артикул',
        poshiv.rst                                'Рост',
        poshiv.kol_sdano                          'Кол-во',
        poshiv.brigada                            'Бригада',
+       poshiv.brigadir                           'Бригадир',
        poshiv.kod_marh                           'Код листа',
        CONVERT(varchar(10), poshiv.data, 104) AS 'Дата пошива'
 FROM Gomel.dbo.nsi_kld nsi_kld
          JOIN Gomel.dbo.poshiv AS poshiv
               ON nsi_kld.kod = poshiv.kod_izd
-WHERE     poshiv.kod_marh = 602529
+WHERE poshiv.kod_marh = 602529
   AND poshiv.kol_sdano > 0
 ORDER BY poshiv.rzm;
